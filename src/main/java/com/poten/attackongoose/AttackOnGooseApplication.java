5a55d4d3a1f74a344ea261aeb86d8b4a60fdeed8
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-11-04 18:18:49 +0900

    init: 🎉 First init DI Setting
    1. Docker 의존성 설정 하지 않음

---
+++ src/main/java/com/poten/attackongoose/AttackOnGooseApplication.java
@@ -0,0 +1,13 @@
+package com.poten.attackongoose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttackOnGooseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttackOnGooseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poten/attackongoose/AttackOnGooseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poten.attackongoose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttackOnGooseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
